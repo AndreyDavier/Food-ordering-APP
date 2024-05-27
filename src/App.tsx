5928da8933760e816ea9e07d2cd4b65839c1a8e0
Andrey <ckastrusshion@gmail.com>
2024-05-27 12:55:51 +0500

    add new components and add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import { useState, MouseEvent } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
+
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Menu />
+	},
+	{
+		path: '/',
+		element: <Cart />
+	}, {
+		path: '*',
+		element: <ErrorPage />
+	}
+]);
 
 function App() {
 
@@ -16,6 +35,14 @@ function App() {
 			<Button onClick={addCounter}>Кнопка</Button>
 			<Button appearenc='big' onClick={addCounter}>Кнопка</Button>
 			<Input placeholder='Email' />
+			<div>
+				<a href="/">Menu</a>
+				<a href="/cart">Корзина</a>
+			</div>
+
+			<RouterProvider router={router}>
+
+			</RouterProvider>
 		</>
 	);
 }
---
+++ src/pages/Menu/Menu.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const Menu: FC = () => {
+	return (
+		<>
+            Menu
+		</>
+	);
+};
