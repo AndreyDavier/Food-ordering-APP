e428df5c6b1286a57371396e2d9912bf00c51d48
Andrey <ckastrusshion@gmail.com>
2024-06-27 16:30:57 +0500

    completed cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -8,9 +8,21 @@ import axios from 'axios';
 import { PREFIX } from '../../helpers/Api';
 import styles from './Cart.module.scss'
 
+const DELIVERY_FEE: number = 169
+
 export const Cart: FC = () => {
 	const [cartProducts, setCartProducts] = useState<Products[]>([])
 	const items = useSelector((s: RootState) => s.cart.items)
+
+	const total = items.map(i => {
+		const product = cartProducts.find(p => p.id === i.id)
+
+		if (!product) {
+			return 0
+		}
+
+		return i.count * product.price
+	}).reduce((acc, i) => acc += i, 0)
 
 	const getItem = async (id: number) => {
 		const { data } = await axios.get<Products>(`${PREFIX}/products/${id}`)
@@ -41,6 +53,20 @@ export const Cart: FC = () => {
 					return <CartItem key={product.id} count={i.count} {...product} />
 				})
 			}
+			<div className={styles["line"]}>
+				<div className={styles["text"]}>Итог</div>
+				<div className={styles["price"]}>{total}<span>&nbsp;₽</span></div>
+			</div>
+			<hr className={styles["hr"]} />
+			<div className={styles["line"]}>
+				<div className={styles["text"]} >Доставка</div>
+				<div className={styles["price"]}>{DELIVERY_FEE}&nbsp;<span>₽</span></div>
+			</div>
+			<hr className={styles["hr"]} />
+			<div className={styles["line"]}>
+				<div className={styles["text"]}>Итог {items.length}</div>
+				<div className={styles["price"]}>Итог {total + DELIVERY_FEE}&nbsp;<span>₽</span></div>
+			</div>
 		</>
 	);
 };
